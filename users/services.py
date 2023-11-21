35e8a217b83ed902e58b24d1e1df163c9bcde49f
Gama_Ray <gamaizingg@gmail.com>
2023-11-21 14:14:41 +0300

    Добавлен метод create вместе с perform_create, а также JsonRespnse для ответа после подтверждения по ссылке

--- users/services.py
+++ users/services.py
@@ -3,12 +3,13 @@ from django.contrib.sites.shortcuts import get_current_site
 from django.core.mail import send_mail
 
 
-def send_confirm_register_mail(self, user, verify_email_url):
+def send_confirm_register_mail(user, absolute_verify_email_url):
+
     send_mail(
         subject='Поздравляем с регистрацией',
         message=f'Вы зарегистрировались на нашей платформе. '
                 f'Для продолжения регистрации перейдите по ссылке '
-                f'{get_current_site(self.request)}{verify_email_url}',
+                f'{absolute_verify_email_url}',
         from_email=settings.EMAIL_HOST_USER,
         recipient_list=[user.email]
     )
--- users/views/views.py
+++ users/views/views.py
@@ -1,6 +1,6 @@
-from django.contrib.messages import success
+from django.http import JsonResponse
 from rest_framework import status
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, get_object_or_404
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -18,35 +18,36 @@ class MyTokenObtainPairAPIView(TokenObtainPairView):
 
 
 class UserRegisterAPIView(CreateAPIView):
+    """Регистрация пользователя"""
     serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = self.perform_create(serializer)
+
+        verify_email_url = reverse('users:verify_email', args=[user.key])
+        absolute_verify_email_url = request.build_absolute_uri(verify_email_url)
+        send_confirm_register_mail(self, user, absolute_verify_email_url)
+
+        return Response({'message': 'Пользователь успешно создан'}, status=status.HTTP_201_CREATED)
 
     def perform_create(self, serializer):
         user = serializer.save()
-        user.set_password(serializer.validated_data['password'])  # Хэширование пароля
+        user.set_password(serializer.validated_data['password'])
         user.key = uuid.uuid4()
         user.save()
-        verify_email_url = reverse('users:verify_email', args=[user.key])
 
-        send_confirm_register_mail(self, user, verify_email_url)
-
-        return Response({success}, status.HTTP_201_CREATED)
-        # email = serializer.validated_data.get('email')
-        # password = serializer.validated_data.get('password')
-        #
-        # user = User.objects.create_user(
-        #     email=email,
-        #     password=password,
-        # )
-        #
-        # serializer.save(email=user.email, password=user.password)
+        return user
 
 
 def verify_email(request, key):
-    user = User.objects.get(key=key)
+    """Подтверждение почты пользователя"""
+    user = get_object_or_404(User, key=key)
 
     if not user.is_active:
         user.is_active = True
         user.save()
-        return Response(status=status.HTTP_200_OK)
+        return JsonResponse({'message': 'Email successfully confirmed'}, status=status.HTTP_200_OK)
     else:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+        return JsonResponse({'message': 'Email already confirmed'}, status=status.HTTP_400_BAD_REQUEST)

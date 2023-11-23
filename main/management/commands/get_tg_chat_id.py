33b98d682e7506404b53419e13a754210c7337c2
Gama_Ray <gamaizingg@gmail.com>
2023-11-23 20:53:39 +0300

    Добавлены printы в get_tg_chat_id

--- main/management/commands/get_tg_chat_id.py
+++ main/management/commands/get_tg_chat_id.py
@@ -9,19 +9,22 @@ class Command(BaseCommand):
     help = 'Команда для получения chat ID из Telegram бота и отправка его обратно.'
 
     def handle(self, *args, **options):
-
         try:
+            print('Запуск команды...')
             url_get = TG_URL.format(TG_BOT_TOKEN, TG_GET_UPDATES)
             url_post = TG_URL.format(TG_BOT_TOKEN, TG_SEND_MESSAGE)
             data = requests.post(url_get)
             response = data.json()
 
             if response['result']:
+                print('Идет отправка сообщения в Telegram...')
                 chat_id = response['result'][-1]['message']['chat']['id']
                 requests.post(url_post, json={
                     'chat_id': chat_id,
                     'text': f'На вот, держи свой чат ID: {chat_id}'
                 })
+                print('Сообщение отправлено.')
 
         except Exception as e:
+            print('Сообщение не отправлено.')
             print(e)
